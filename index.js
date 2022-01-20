5397903d64de7170b86dd91abbc65636c4fbd1c8
BridgetAkellian <akellianbv@gmail.com>
2022-01-20 19:13:14 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const cards = ["Charlie", "Samip", "Ali"];
+
+function writeCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+      console.log(`Thank you, ${cards[i]}, for the wonderful birthday gift`);
+      debugger;
+    }
+  
+    return cards;
+  }
+  
+  writeCards(cards);
+
+// countdown 
+
+let countdown = 10;
+while (countdown > 0) {
+  console.log(countdown--);
+}
